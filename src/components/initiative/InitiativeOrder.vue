f84394075bf72dc7f868de4db252a7bdda79a2d0
jeanbono <pierre.fraisse@nebulous.fr>
2025-04-12 14:49:39 +0200

    Turn color

--- src/components/initiative/InitiativeOrder.vue
+++ src/components/initiative/InitiativeOrder.vue
@@ -310,7 +310,7 @@ function startNewCombat() {
           <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-purple-700 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
-          <span class="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-indigo-600">
+          <span class="text-4xl font-bold text-purple-700">
             Tour {{ turnStore.currentTurn }}
           </span>
         </div>
